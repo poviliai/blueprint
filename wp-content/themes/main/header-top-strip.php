ea3a41334f1d91a5ec8fc623a966c52085d8a0f0
poviliai <v.poviliai@gmail.com>
2024-09-01 12:05:23 +0300

    top strip header

--- wp-content/themes/main/header-top-strip.php
+++ wp-content/themes/main/header-top-strip.php
@@ -1,8 +1,8 @@
 <div class="bg-light mb-2 mb-sm-4 py-1 top-strip">
     <div class="container">
        <div class="d-flex justify-content-center align-items-between justify-content-md-between align-items-md-center flex-column flex-md-row">
-           <div class="d-flex justify-content-center justify-content-sm-start"> <a class="me-2 col-6 col-sm-auto text-end text-sm-start"  href="#">Lorem Ipsum</a>|<a class="ms-2 col-6 col-sm-auto" href="#">Lorem Ipsum</a>  </div>
-           <div class="d-flex justify-content-center justify-content-sm-end"> <a class="me-2 col-6 col-sm-auto text-end text-sm-start" href="tel:+8 800 00 00 000">+ 8 800 00 00 000</a>|<a class="ms-2 col-6 col-sm-auto" href="mailto:example@mail.com">example@mail.com</a> </div>
-       </div>
+            <div class="d-flex justify-content-center justify-content-sm-end align-items-center"> <a class="me-2 col-6 col-sm-auto text-end text-sm-start" href="tel:+8 800 00 00 000"><span class="dashicons dashicons-phone mt-1"></span> + 8 800 00 00 000</a>|<span class="dashicons dashicons-email-alt ms-1"></span> <a class="ms-2 col-6 col-sm-auto" href="mailto:example@mail.com">example@mail.com</a> </div>
+            <div class="d-flex justify-content-center justify-content-sm-start align-items-center"> <a class="me-2 col-6 col-sm-auto text-end text-sm-start"  href="#">Login <span class="dashicons dashicons-admin-network mt-1"></span></a>|<a class="ms-2 col-6 col-sm-auto" href="#">Register <span class="dashicons dashicons-admin-users mt-1"></span></a></div>
+        </div>
     </div>
 </div>
